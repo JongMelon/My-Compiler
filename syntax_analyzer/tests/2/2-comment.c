a5d15fb57ec5736c13df8f0f1d47ab4504a0daa2
JongMelon <2689526133@qq.com>
2024-06-04 21:54:16 +0800

    Test cases for compiler

---
+++ syntax_analyzer/tests/2/2-comment.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*/skipher/*/
+//int main(){
+int main(){
+	////return 0;}/*
+	/*}
+	//}return 1;*/
+	//}return 2;*//*
+	printf("There are some comments in this code.\n");
+	return 3;
+	//*/
+}
+//
